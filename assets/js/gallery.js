9f7cca530a57e98769b499260969a297fd8a48d4
Sofía J <sofiajimglez@gmail.com>
2023-01-07 23:47:21 +0100

    Gallery page

---
+++ assets/js/gallery.js
@@ -0,0 +1,21 @@
+const popup = document.getElementById('popup');
+
+const selectedImage = document.getElementById('selected-img');
+const selectedIndex = null;
+
+const imageIndexes = document.querySelectorAll('.gallery-img');
+
+imageIndexes.forEach((img) => {
+    img.addEventListener('click', () => {
+        popup.style.transform = 'translateY(0)';
+        selectedImage.src = img.src;
+        selectedImage.alt = img.alt;
+    })
+});
+
+popup.addEventListener('click', () => {
+    popup.style.transform = 'translateY(-100%)';
+    popup.src = '';
+    popup.alt = '';
+})
+
