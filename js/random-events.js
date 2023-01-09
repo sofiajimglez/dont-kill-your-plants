6596f6dd5eca0c98c7aa5874da176f3bf3f7661e
Sofía J <sofiajimglez@gmail.com>
2023-01-09 22:13:55 +0100

    How to play page

--- js/random-events.js
+++ js/random-events.js
@@ -1,6 +1,7 @@
 function randomEventCat(plant) {
     plant.health = plant.health - 5;
     alert('Oh no! The cat has thrown the plant to the ground! :(');
+    //Swal.fire('Any fool can use a computer');
 };
 
 function randomEventHappy(plant) {
@@ -10,16 +11,19 @@ function randomEventHappy(plant) {
         plant.health = plant.health + 2;
     }
     alert('Your plant woke up very happy today! Nice!');
+    //Swal.fire('Any fool can use a computer')
 };
 
 function randomEventMom(plant) {
     if (plant.water + 1 > 10) {
         plant.water = 10;
         plant.health -= 1;
-        alert('Your mother has watered the plant more than desirable! MOM!!! >:(');
+        //alert('Your mother has watered the plant more than desirable! MOM!!! >:(');
+        Swal.fire('Any fool can use a computer')
       } else {
         plant.water += 1;
         alert('Your mother has remembered to water your plants, thank you mommy!');
+        //Swal.fire('Any fool can use a computer')
       };
     
 };
@@ -28,4 +32,5 @@ function randomEventPlague(plant) {
     plant.health = plant.health - 1;
     plant.illness = true;
     alert('The plant has been attacked by a fungal infection... :(');
+    //Swal.fire('Any fool can use a computer')
 };
